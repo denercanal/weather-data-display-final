88b7a7f41d8e155de429c93e5e36ec1f8dbafc68
Dener Canal <dener.canal@itixti.com.br>
2021-05-06 01:41:23 -0300

    observer na tabela, ok

--- src/main/java/br/ufes/pss/weather_data_display_final/presenter/PresenterTabelaTempo.java
+++ src/main/java/br/ufes/pss/weather_data_display_final/presenter/PresenterTabelaTempo.java
@@ -7,10 +7,11 @@ import br.ufes.pss.weather_data_display_final.observer.ITempoObservador;
 import br.ufes.pss.weather_data_display_final.view.ViewTabelaTempo;
 import br.ufes.pss.weather_data_display_final.view.ViewTelaPrincipal;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
-public class PresenterTabelaTempo implements ITempoObservador{
+public class PresenterTabelaTempo implements ITempoObservador {
 
     private ViewTelaPrincipal viewTelaPrincipal;
     private ViewTabelaTempo viewTabelaTempo;
@@ -19,11 +20,11 @@ public class PresenterTabelaTempo implements ITempoObservador{
     PresenterTabelaTempo(ViewTelaPrincipal viewTelaPrincipal) {
         this.viewTelaPrincipal = viewTelaPrincipal;
         this.viewTabelaTempo = new ViewTabelaTempo();
-        
+
         this.viewTelaPrincipal.getDesktop().add(viewTabelaTempo);
 
         this.businessTempo = new BusinessTempo();
-        
+
         this.viewTabelaTempoRemover();
     }
 
@@ -44,7 +45,21 @@ public class PresenterTabelaTempo implements ITempoObservador{
 
     @Override
     public void update(TempoCollection tempoCollection) {
-        ArrayList<Tempo> tempos = TempoCollection.getTempoCollection().getAllTempos();
 
+        try {
+            var tempos = TempoCollection.getTempoCollection().getAllTempos();
+
+            ((DefaultTableModel) this.viewTabelaTempo.getTabela().getModel()).setNumRows(0);
+            viewTabelaTempo.getTabela().updateUI();
+
+            DefaultTableModel model = (DefaultTableModel) viewTabelaTempo.getTabela().getModel();
+
+            if (!tempos.isEmpty()) {
+                for (Tempo tempo : tempos) {
+                    model.addRow(new Object[]{tempo.getDataTempo().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")), tempo.getTemperaturaTempo(), tempo.getUmidadeTempo(), tempo.getPressaoTempo()});
+                }
+            }
+        } catch (Exception e) {
+        }
     }
 }
